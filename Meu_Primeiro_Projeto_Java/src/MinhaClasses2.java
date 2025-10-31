772ba4d30da30addc9014bad3cd9504d0dfda931
Luiz-Henrique-Vieira <luizh4544@gmail.com>
2025-10-31 11:41:09 -0300

    MinhaClasse.2

---
+++ Meu_Primeiro_Projeto_Java/src/MinhaClasses2.java
@@ -0,0 +1,14 @@
+public class MinhaClasses2 {
+    public static void main(String[] args) {
+        final String NM = "Luiz Henrique";
+        double MT = 1.80;
+        int ANOS_DE_VIDA = 20;
+        int ANO_2025 = 2025;
+        System.out
+                .print("Ola me chamo " + NM + "\n" + "Tenho " + MT + " de altura" + "\n" + "Tenho " + ANOS_DE_VIDA
+                        + " anos de idade"
+                        + "\n" + "Estou realizando o curso da DIO.ME de Java Básico no ano " + ANO_2025);
+
+    }
+
+}
