20ef9dcb6b0486d6cf184f6d3a6186be6211ff01
Luiz-Henrique-Vieira <luizh4544@gmail.com>
2025-10-31 10:59:05 -0300

    MinhaPrimeiraClasse

---
+++ Meu_Primeiro_Projeto_Java/src/MinhaClasse.java
@@ -0,0 +1,8 @@
+public class MinhaClasse {
+
+    public static void main(String[] args) {
+        System.out.print("Olá, tudo bem? Me chamo Luiz Henrique! E esse é o meu primeiro código em Java!");
+
+    }
+
+}
