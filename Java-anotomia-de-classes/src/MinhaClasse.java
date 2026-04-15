1d9c313780b62923a4b52645179048a427e6d887
Luiz-Henrique-Vieira <150498365+Luiz-Henrique-Vieira@users.noreply.github.com>
2026-04-15 13:41:50 +0000

    Atualização de dados

--- Java-anotomia-de-classes/src/MinhaClasse.java
+++ Java-anotomia-de-classes/src/MinhaClasse.java
@@ -1,7 +1,7 @@
 public class MinhaClasse {
 
     public static void main (String[] args){
-        System.out.print("Olá, senhor Luiz Henrique");
+        System.out.print("Olá, senhor Luiz Henrique ");
     }
     
 }
